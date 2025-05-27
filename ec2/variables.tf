b06f4f1d2fd134417fe95474eb0147ab22561231
Mariam Mohamed Hegazy <mariamhegazy550@gmail.com>
2025-05-27 05:32:19 +0300

    Add files via upload

---
+++ ec2/variables.tf
@@ -0,0 +1,23 @@
+variable "ami" {
+  type = string
+  description = "EC2 ami"
+}
+
+variable "type" {
+  
+}
+
+variable "sub_id" {
+  
+}
+
+variable "key_name" {
+  
+}
+variable "vpc_id" {
+  
+}
+
+variable "key_path" {
+  
+}
